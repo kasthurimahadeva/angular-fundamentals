06c691b058f89cf7597732f927b34063b2f5e475
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2019-06-25 04:32:42 +0530

    [ngSwitch]

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -5,9 +5,14 @@ import {Component, Input} from '@angular/core';
     <div class="well hoverwell thumbnail">
       <h2>{{event?.name}}</h2>
       <div>Date: {{event?.date}}</div>
-      <div>Time: {{event?.time}}</div>
+      <div [ngSwitch]="event?.time">
+        Time: {{event?.time}}
+        <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+        <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+        <span *ngSwitchDefault>(Normal Start)</span>
+      </div>
       <div>Price: \${{event?.price}}</div>
-      <div [hidden]="!event?.location">
+      <div *ngIf="event?.location">
         <span class="pad-right">Location: {{event.location?.address}}</span>
         <span>{{event.location?.city}}, {{event.location?.country}}</span>
       </div>
