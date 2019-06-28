38f7bb0dcbc27697f67a136a46f25902c8eb51cd
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2019-06-28 23:24:10 +0530

    custom validators

--- src/app/events/create-session/create-session.component.ts
+++ src/app/events/create-session/create-session.component.ts
@@ -22,7 +22,7 @@ export class CreateSessionComponent implements OnInit {
     this.presenter = new FormControl('', Validators.required);
     this.duration = new FormControl('', Validators.required);
     this.level = new FormControl('', Validators.required);
-    this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400)]);
+    this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400), this.restrictedWords]);
 
     this.newSessionForm = new FormGroup({
       name: this.name,
@@ -31,6 +31,10 @@ export class CreateSessionComponent implements OnInit {
       level: this.level,
       abstract: this.abstract
     });
+  }
+
+  private restrictedWords(control: FormControl): {[key: string]: any} {
+    return control.value.includes('foo') ? {'restrictedWords': 'foo'} : null;
   }
 
   saveSession(formValues) {
