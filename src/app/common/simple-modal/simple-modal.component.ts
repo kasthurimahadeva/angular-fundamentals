3f9719058f3d974c36e7c654c3a059f21753ffa1
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2019-07-06 00:17:57 +0530

    creating settings on component

--- src/app/common/simple-modal/simple-modal.component.ts
+++ src/app/common/simple-modal/simple-modal.component.ts
@@ -10,6 +10,7 @@ export class SimpleModalComponent implements OnInit {
 
   @Input() title: string;
   @Input() elementId: string;
+  @Input() closeOnBodyClick: string;
   @ViewChild('modalcontainer') containerEl: ElementRef;
   constructor(@Inject(JQ_TOKEN) private $: any) { }
 
@@ -17,6 +18,8 @@ export class SimpleModalComponent implements OnInit {
   }
 
   closeModal() {
-    this.$(this.containerEl.nativeElement).modal('hide');
+    if (this.closeOnBodyClick.toLocaleLowerCase() === 'true') {
+      this.$(this.containerEl.nativeElement).modal('hide');
+    }
   }
 }
